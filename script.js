6fe0c156a518a0f918f9fc1647c91612d31c5c12
Kenji <k_kimura@mail.toyota.co.jp>
2025-05-17 23:09:01 +0900

    修正

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-
-function alertMessage() {
-  alert(
-    "購入品名称： " + document.getElementById("myForm").purchasedItems.value + "\n" +
-    "金額： " + document.getElementById("myForm").amount.value + "\n" +
-    "勘定科目： " + document.getElementById("myForm").accountItem1.value
-  );
-}
-
-// プルダウン要素を取得
-
+// プルダウン要素の変更イベントを取得
 document.addEventListener("DOMContentLoaded", function() {
   const selectElement = document.getElementById("accountItem1");
   selectElement.addEventListener("change", function() {
     alert("選択された値は： " + this.value);
   });
 });
+
+// 入力内容を表示
+function alertMessage() {
+  alert(
+    "購入品名称： " + document.getElementById("myForm").purchasedItems.value + "\n" +
+    "金額： " + document.getElementById("myForm").amount.value + "\n" +
+    "勘定科目： " + document.getElementById("accountItem1").accountItem1.value
+  );
+}
 
