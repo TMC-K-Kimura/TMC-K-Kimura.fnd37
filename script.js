db88aad9dae085f8992d121c5d0ef41fd08f3854
Kenji <k_kimura@mail.toyota.co.jp>
2025-05-17 22:19:07 +0900

    修正

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function alert_message() {
   alert(
     "購入品名称： " + document.getElementById("myForm").purchasedItems.value + "\n" +
     "金額： " + document.getElementById("myForm").ammount.value + "\n" +
-    "勘定科目： " + document.getElementById("myForm").accountItem1.value + "\n" +
+    "勘定科目： " + document.getElementById("myForm").accountItem1.value
   );
 }
 
