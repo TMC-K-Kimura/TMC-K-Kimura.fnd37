cd532ecce3a2b88574cf2692d633f63e5022af54
Kenji <k_kimura@mail.toyota.co.jp>
2025-05-17 23:03:57 +0900

    syuusei

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 // 1行目に記載している 'use strict' は削除しないでください
 
 
-function alertmessage() {
+function alertMessage() {
   alert(
     "購入品名称： " + document.getElementById("myForm").purchasedItems.value + "\n" +
-    "金額： " + document.getElementById("myForm").ammount.value + "\n" +
+    "金額： " + document.getElementById("myForm").amount.value + "\n" +
     "勘定科目： " + document.getElementById("myForm").accountItem1.value
   );
 }
@@ -14,7 +14,6 @@ function alertmessage() {
 
 document.addEventListener("DOMContentLoaded", function() {
   const selectElement = document.getElementById("accountItem1");
-
   selectElement.addEventListener("change", function() {
     alert("選択された値は： " + this.value);
   });
